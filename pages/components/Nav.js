a97f214e9086cf6bb121b2ab71e6c70032e6cf12
Alice Xia <alicexia@alices-mbp.myfiosgateway.com>
2021-02-05 00:39:01 -0500

    font weight

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -1,5 +1,3 @@
-import styled from 'styled-components';
-
 export default function Nav({ recipe }) {
   return (
     <div className="nav">
@@ -14,29 +12,3 @@ export default function Nav({ recipe }) {
     </div>
   );
 }
-
-const Navigation = styled.nav`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  padding: 1rem 3rem;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  h2 {
-    font-size: 3rem;
-    margin: 0px;
-  }
-`;
-
-const Links = styled.div`
-  display: flex;
-  
-  a {
-    margin-left: 2rem;
-    color: #1421A3;
-    font-size: 0.9rem;
-  }
-`;
